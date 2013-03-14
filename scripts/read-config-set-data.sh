6632d454a4d602d7a1afe187d37266c2af864f5f
Magnus Feuer <magnus@feuerlabs.com>
2013-03-14 14:51:34 -0700

    Added sanity checks and descriptive help for config set area.

--- scripts/read-config-set-data.sh
+++ scripts/read-config-set-data.sh
@@ -4,7 +4,14 @@
 
 if [ $# != 2 ]
 then
-    echo "Usage: $0 config-set area"
+    echo "Usage: $0 config-set [staged|pending|installed]"
+    exit 255
+fi
+
+if [ "$2" != "staged" -a "$2" != "pending" -a "$2" != "installed" ]
+then
+    echo "Please specify staged, pending, or installed for the area of the "
+    echo "configuration set you want to examine."
     exit 255
 fi
 
